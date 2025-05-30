3b02240becf859adb7a537c375f2a4eea350aea4
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-05-30 11:23:13 -0300

    definição de constructor e método next

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,37 @@
-
-
-//carousel
-
-//Array storage class
 let carouselArr = [];
 
-
-//class Carousel
 class Carousel {
-
-    
-      
-    static Start(arr){
-        if(arr){
-
-            if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-            }
-            
-        } else {
-            throw "Method Start need a Array Variable.";
-        }
+    constructor(imagem, titulo, link) {
+        this.imagem = imagem;
+        this.titulo = titulo;
+        this.link = link;
     }
 
-    static Next(){
-        
+  static Start(arr) {
+    if (arr && arr.length > 0) {
+      Carousel._sequence = 0;
+      Carousel._size = arr.length;
+      Carousel.Next(); //start
+      Carousel._interval = setInterval(function() {
+        Carousel.Next();
+      }, 5000);
+    } else {
+      throw "Method Start need a Array Variable.";
     }
-};
+  }
+
+  static Next() {
+    const item = carouselArr[Carousel._sequence];
+
+    const carouselDiv = document.querySelector("#carousel");
+    const titleDiv = document.querySelector("#carousel-title");
+
+    carouselDiv.innerHTML = `<img src="img/${item.imagem}">`;
+    titleDiv.innerHTML = `<a href="${item.link}" target="_blank">${item.titulo}</a>`;
+
+    Carousel._sequence++;
+    if(Carousel._sequence >= Carousel._size) {
+        Carousel._sequence = 0;
+    }
+  }
+}
